97bedeb6972c32f187950cb90a035132197f5d90
Jungsu Han <jshan@smartx.kr>
2020-12-18 02:42:45 +0900

    add

---
+++ coding_interview/36.py
@@ -0,0 +1,20 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        
+        results = []
+        
+        def dfs (csum, index, path):
+            
+            if csum <0:
+                return
+            if csum ==0:
+                results.append(path)
+                return
+            
+            for i in range (index, len(candidates)):
+                dfs (csum -candidates[i], i, path+[candidates[i]])
+                
+        
+        dfs (target, 0, [])
+        return results
+        
