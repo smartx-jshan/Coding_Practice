06b8ad7a3b1f50b034df3f5f7f16b45d7d8e6f89
Jungsu Han <jshan@smartx.kr>
2020-12-21 15:01:31 +0900

    add

---
+++ coding_interview/40.py
@@ -0,0 +1,25 @@
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], N: int, K: int) -> int:
+        
+        graph = collections.defaultdict(list)
+        for u, v, w in times:
+            graph[u].append((v,w))
+        
+        Q = [(0,K)]
+        
+        dist = collections.defaultdict(list)
+        
+        while Q:
+            time, node = heapq.heappop(Q)
+            if node not in dist:
+                dist[node] = time
+            
+                for v, w in graph[node]:
+                    alt = time + w
+                    heapq.heappush(Q, (alt,v))
+        
+        if len(dist) !=N:
+            return -1
+        
+        return max(dist.values())
+    
