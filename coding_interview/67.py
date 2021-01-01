f4d46a45561816dcac95306e2909847b4869d413
Jungsu Han <jshan@smartx.kr>
2021-01-01 18:16:21 +0900

    add

---
+++ coding_interview/67.py
@@ -0,0 +1,21 @@
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        
+        nums1.sort()
+        nums2.sort()
+        result = set()
+        
+        i = 0
+        j = 0
+        
+        while i < len(nums1) and j< len(nums2):
+            if nums1[i] < nums2[j]:
+                i +=1
+            elif nums1[i] > nums2[j]:
+                j +=1
+            else:
+                result.add(nums1[i])
+                i +=1
+                j +=1
+        
+        return result
