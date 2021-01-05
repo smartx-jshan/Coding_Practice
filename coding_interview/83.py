3239d0700ac3732af1a05eff8faecd65f7ec0c8e
Jungsu Han <jshan@smartx.kr>
2021-01-06 03:41:14 +0900

    add

---
+++ coding_interview/83.py
@@ -0,0 +1,21 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        
+        if not nums:
+            return None
+        
+        if len(nums) == 1:
+            return nums[0]
+        
+        half = len(nums)//2
+        
+        a= self.majorityElement(nums[:half])
+        b= self.majorityElement(nums[half:])
+        
+        if nums.count(a) > half:
+            return a
+        else:
+            return b
+        
+        #return [b,a][nums.count(a) > half]
+    
