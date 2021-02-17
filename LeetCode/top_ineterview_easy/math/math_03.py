506e1b92b5317154a09d85cd56bbbae733501f25
Jungsu Han <jshan@smartx.kr>
2021-02-17 15:21:59 +0900

    delete

--- LeetCode/top_ineterview_easy/math/math_03.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def isPowerOfThree(self, n: int) -> bool:
-        
-        return n > 0 and pow(3, 31, n) == 0
-
-        
-        
-        
--- LeetCode/top_ineterview_easy/others/others_02.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def hammingDistance(self, x: int, y: int) -> int:
-        
-        # x XOR y and count '1'
-        return bin(x ^ y ).count('1')
-    
